196d7200d7f72175c88d2d5140c910525225db53
geomio <geoff.millner22@gmail.com>
2021-02-05 10:35:02 -0800

    added clickable js code to project

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+jQuery("h1").click(function() {
+  alert("This is a header.");
+});
+
+jQuery("p").click(function() {
+  alert("This may be a paragraph?");
+});
+
+jQuery("img").click(function() {
+  alert("This is not a Image its a dolphin.");
+});
